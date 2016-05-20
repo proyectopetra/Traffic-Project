8f37d12575c463874d78474137c52361ea404a74
unintendedbear <unintendedbear@gmail.com>
2016-05-20 16:27:52 +0200

    Handling an error that appeared when no information was available from a node

--- dgtScrapper/scrapDGT.py
+++ dgtScrapper/scrapDGT.py
@@ -31,21 +31,18 @@ def getSensorData(_nombre,_elemGenCod):
 	print theUrl
 	page = html.fromstring(urllib.urlopen(theUrl).read())
 	lis = page.xpath("//li")
-	intensidad = lis[1].text_content().split(" ")[2]
-	velocidad = lis[2].text_content().split(" ")[3]
-	ocupacion = lis[3].text_content().split(" ")[3]
-	ligeros	= lis[4].text_content().split(" ")[2]
+	if len(lis) > 2:
+		intensidad = lis[1].text_content().split(" ")[2]
+		velocidad = lis[2].text_content().split(" ")[3]
+		ocupacion = lis[3].text_content().split(" ")[3].encode("ascii")
+		ligeros	= lis[4].text_content().split(" ")[2]
 
 	return [intensidad,velocidad,ocupacion,ligeros]
 
-#EJEMPLO DE USO	
+#EJEMPLO DE USO
 afs = getAforadores("18","Granada")
 
 for element in afs:
 	print element[0],element[1]
 	print getSensorData(element[0],element[1])
 	print " "
-
-
-
-
